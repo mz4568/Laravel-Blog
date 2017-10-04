b1abda303a2c665fa9dfcc99fcc4d86ba9e12601
mz4568 <mz4568@users.noreply.github.com>
2017-10-05 01:24:13 +0600

    Add files via upload

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('partials._head')
+   
+  </head>
+  <body>
+  @include('partials._nav')
+ 
+<div class='container'>
+  @include('partials._message')
+  @yield('content')
+  
+  
+  @include('partials._footer')
+</div>  <!--container-->
+
+  @include('partials._javascript')
+ 
+  </body>
+ 
+</html>
+
+ 
